3f03f8cb81514e7fce8d403ddf033c50feec3b90
AmandaMatar <amandamatar@gmail.com>
2023-03-06 16:50:47 -0300

    Commits do projeto

---
+++ dio-desafio-padroes-projeto/src/facade/TesteFacade.java
@@ -0,0 +1,9 @@
+package facade;
+
+public class TesteFacade {
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+        facade.migrarCliente("Venilton", "14801788");
+    }
+}
+
